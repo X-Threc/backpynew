8f15ca212ef3544ad039f0a6d6e95d757b639025
X-Threc <79155566+X-Threc@users.noreply.github.com>
2022-04-12 16:56:23 +0300

    create first model

--- djangobackend/imagesData/models.py
+++ djangobackend/imagesData/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class ImageDataTo(models.Model):
+    image_url =models.ImageField(upload_to ='uploads/')
+    from_language = models.CharField("From Language", max_length=100)
+    to_language = models.CharField("To Language", max_length=100)
+
