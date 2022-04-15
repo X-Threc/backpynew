177ea336af6cfca84f51c52f44c2392a93ffaa59
X-Threc <79155566+X-Threc@users.noreply.github.com>
2022-04-15 12:37:14 +0300

    add axios ImageService

---
+++ djangobackend/frontend/src/ImageService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:8000';
+
+export default class ImageService{
+	
+	constructor(){}
+	
+	
+	// getCustomers() {
+	// 	const url = `${API_URL}/api/customers/`;
+	// 	return axios.get(url).then(response => response.data);
+	// }  
+	// getCustomersByURL(link){
+	// 	const url = `${API_URL}${link}`;
+	// 	return axios.get(url).then(response => response.data);
+	// }
+	// getCustomer(pk) {
+	// 	const url = `${API_URL}/api/customers/${pk}`;
+	// 	return axios.get(url).then(response => response.data);
+	// }
+	// deleteCustomer(customer){
+	// 	const url = `${API_URL}/api/customers/${customer.pk}`;
+	// 	return axios.delete(url);
+	// }
+    // updateCustomer(customer){
+	// 	const url = `${API_URL}/api/customers/${customer.pk}`;
+	// 	return axios.put(url,customer);
+	// }
+	createCustomer(imageData){
+		const url = `${API_URL}/api/customers/`;
+		return axios.post(url,imageData);
+	}
+
+}
