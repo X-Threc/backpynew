db14135c71be615aca7d2f74c7a0cf34f0b9977a
X-Threc <79155566+X-Threc@users.noreply.github.com>
2022-04-28 13:53:45 +0300

    update select language

--- djangobackend/frontend/src/ImageDisplay.js
+++ djangobackend/frontend/src/ImageDisplay.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-
+import Select from 'react-select'
 import ImageService from './ImageService';
 
 const imageService = new ImageService();
+
+const options = [
+  { value: 'ru', label: 'Русский' },
+  { value: 'en', label: 'Английский' },
+  { value: 'uk', label: 'Украинский' }
+]
+
 
 class ImageDisplay extends Component {
   constructor(props) {
@@ -12,7 +19,9 @@ class ImageDisplay extends Component {
     this.state = {
       text: '',
       image: null,
-      imagePreviewUrl: ''
+      imagePreviewUrl: '',
+      fromLanguage: null,
+      toLanguage: null
     };
   }
 
@@ -30,14 +39,28 @@ class ImageDisplay extends Component {
   };
 
 
+  handleFromLangChange = (fromLanguage1) => {
+    this.setState({fromLanguage: fromLanguage1.value
+    }, () =>
+      console.log(`Option selected:`, this.state.fromLanguage)
+    );
+  };
+
+  handleToLangChange = (toLanguage1) => {
+    this.setState({toLanguage: toLanguage1.value
+    }, () =>
+      console.log(`Option selected:`, this.state.toLanguage)
+    );
+  };
 
   handleSubmit(event){
     event.preventDefault();
     console.log(this.state);
     let form_data = new FormData();
     form_data.append('image_url', this.state.image, this.state.image.name);
-    form_data.append('from_language', this.refs.fromLanguage.value);
-    form_data.append('to_language', this.refs.toLanguage.value);
+    form_data.append('from_language', this.state.fromLanguage);
+    form_data.append('to_language', this.state.toLanguage);
+
     imageService.postImageData(form_data, {
       headers: {
         'content-type': 'multipart/form-data'
@@ -50,10 +73,14 @@ class ImageDisplay extends Component {
   
 
   render() {
-    console.log(this.state.text)
+    console.log(this.state.fromLanguage)
     let text = this.state.text;
+    let fromLanguage = this.state.fromLanguage;
+    let toLanguage = this.state.toLanguage;
     let imagePreviewUrl = this.state.imagePreviewUrl
     let $imagePreview = null;
+
+
     if(imagePreviewUrl){
       $imagePreview = (<img src={imagePreviewUrl} />);
     } else {
@@ -66,17 +93,27 @@ class ImageDisplay extends Component {
     } else {
       $textPreview = (<p>Нет текста</p>);
     }
+
     return (
       <div className="previewComponent">
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label>
-              From Language:</label>
-            <input className="form-control" type="text" ref='fromLanguage' />
+              Перевести с:</label>
+            {/* <input className="form-control" type="text" ref='fromLanguage' /> */}
+            <Select 
+              onChange={this.handleFromLangChange}
+              options={options}
+              />
+              
 
             <label>
-              To Language:</label>
-            <input className="form-control" type="text" ref='toLanguage' />
+              Перевести на:</label>
+            {/* <input className="form-control" type="text" ref='toLanguage' /> */}
+            <Select 
+              onChange={this.handleToLangChange}
+              options={options}/>
+
 
             <input className="imageInput"
               type="file"
